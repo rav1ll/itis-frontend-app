5f1b8cdffcd219e822cf4a5bba261cb880e2cd06
ghost-str <email@example.com>
2022-02-27 19:42:44 +0300

    done

---
+++ src/components/formFields/TextField.js
@@ -0,0 +1,10 @@
+import React from "react";
+import BaseWrapper from "./BaseWrapper";
+import BaseInput from "../inputs/BaseInput";
+
+
+export default function TextField({label, ...props}) {
+    return <BaseWrapper label={label}>
+        <BaseInput {...props} />
+    </BaseWrapper>
+}
