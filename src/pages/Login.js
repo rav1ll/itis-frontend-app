275ea3ded0133e1ab0990e7f335078dffbbb8239
ghost-str <email@example.com>
2022-02-27 15:55:02 +0300

    created button sceleton

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+
+import Button from "../components/input/Button";
+
+
+const LoginButton = styled(Button)`
+  background-color: aqua;
+`
+
 
 export default function Login() {
-    return <div>Login page</div>
+    return <div>
+        Login page
+        <LoginButton />
+    </div>
 }
