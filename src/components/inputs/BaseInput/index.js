ba7b9c8fcd6d922ded089463d960ed3504e54003
ghost-str <email@example.com>
2022-02-27 17:23:03 +0300

    input sceleton

---
+++ src/components/inputs/BaseInput/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import {StyledInput} from "./components";
+
+
+export default function BaseInput() {
+    return <StyledInput/>
+}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,11 +3,16 @@ import styled from "styled-components";
 
 import Button from "../components/inputs/Button";
 import DefaultLayout from "../components/DefaultLayout/DefaultLayout";
+import BaseInput from "../components/inputs/BaseInput";
 
 const LoginButton = styled(Button)`
   font-size: 2em;
 `
 
 export default function Login() {
-    return <DefaultLayout title={'Login Page'}>Login page <LoginButton >Log in</LoginButton></DefaultLayout>
+    return <DefaultLayout title={'Login Page'}>
+        Login page
+        <BaseInput/>
+        <LoginButton >Log in</LoginButton>
+    </DefaultLayout>
 }
