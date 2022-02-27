ae9bc325fdaf66cfb0784603dd1060150b64f550
ghost-str <email@example.com>
2022-02-27 17:37:37 +0300

    created input

--- src/components/inputs/BaseInput/components.js
+++ src/components/inputs/BaseInput/components.js
@@ -4,3 +4,12 @@ export const StyledInput = styled.input`
   border: unset;
   outline: unset;
 `;
+
+export const Container = styled.div`
+  display: inline-block;
+  padding: 0.2em;
+  border: 3px solid #379683;
+  border-radius: 6px;
+`
+
+export const SlotContainer = styled.div``;
--- src/components/inputs/BaseInput/index.js
+++ src/components/inputs/BaseInput/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 
-import {StyledInput} from "./components";
+import {Container, SlotContainer, StyledInput} from "./components";
 
 
-export default function BaseInput() {
-    return <StyledInput/>
+export default function BaseInput({className, leftSlot, rightSlot, ...props}) {
+    return <Container className={className}>
+        {leftSlot && <SlotContainer>{leftSlot}</SlotContainer>}
+        <StyledInput {...props}/>
+        {rightSlot && <SlotContainer>{rightSlot}</SlotContainer>}
+    </Container>
 }
