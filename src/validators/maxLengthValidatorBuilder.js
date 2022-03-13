3e0278ece9271479a74c41e5d010a31f4e1b9e82
DmitriyFirsov <dmitriy.firsov@flatstack.com>
2022-03-13 23:55:16 +0300

    fix max length validator

--- src/validators/maxLengthValidatorBuilder.js
+++ src/validators/maxLengthValidatorBuilder.js
@@ -1,5 +1,5 @@
 const maxLengthValidatorBuilder = (length)=> (value) => {
-    return value.length <= length ? '' : `Field must contain less than ${length} symbols`;
+    return value.trim().length <= length ? '' : `Field must contain less than ${length} symbols`;
 }
 
 export default maxLengthValidatorBuilder;
