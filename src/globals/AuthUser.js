2f90695e49b166a3ee082e6d7b148cef136dd142
DmitriyFirsov <dmitriy.firsov@flatstack.com>
2022-03-14 04:11:07 +0300

    context reworcked

--- src/globals/AuthUser.js
+++ src/globals/AuthUser.js
@@ -1,11 +1,34 @@
-import React, { createContext, useMemo, useState } from 'react';
+import React, { createContext, useContext, useMemo, useReducer } from 'react';
 
-export const UserContext = createContext({ user: null, isLoading: null });
+const INITIAL_STATE = { user: null, isLoading: null };
+
+const UserContext = createContext();
+
+function reducer(state, action) {
+	switch (action.type) {
+		case 'update':
+			return {
+				...state,
+				user: action.payload
+			};
+		case 'logout':
+			return {
+				...state,
+				user: null
+			};
+		default:
+			return state;
+	}
+}
 
 export function AuthUser({ children }) {
-	const [user, setUser] = useState({ user: null, isLoading: null });
+	const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
 
-	const value = useMemo(() => ({ ...user, setUser }), [user]);
+	const value = useMemo(() => ({ state, dispatch }), [state]);
 
 	return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+}
+
+export default function useAuthUser() {
+	return useContext(UserContext);
 }
