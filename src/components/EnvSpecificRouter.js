2cccb2b0ca79334ec6bd8d699fd0ee8e5a392bb3
DmitriyFirsov <dmitriy.firsov@flatstack.com>
2022-03-20 02:40:44 +0500

    fix router on github pages

--- src/components/EnvSpecificRouter.js
+++ src/components/EnvSpecificRouter.js
@@ -4,5 +4,5 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 // for BrowserRouter server for all request should return index page
 export default function EnvSpecificRouter({ children }) {
 	const Router = process.env.REACT_APP_HASH_ROUTER === 'true' ? HashRouter : BrowserRouter;
-	return <Router basename={process.env.PUBLIC_URL}>{children}</Router>;
+	return <Router basename={process.env.REACT_APP_HASH_ROUTER === 'true' ? undefined : process.env.PUBLIC_URL}>{children}</Router>;
 }
