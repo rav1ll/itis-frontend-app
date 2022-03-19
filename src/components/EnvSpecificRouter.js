d1f7e6173c1802b9a6ba86faa4dae80c31af7703
DmitriyFirsov <78723252+DmitriyFirsov@users.noreply.github.com>
2022-03-20 00:51:43 +0500

    Env varible HASH_ROUTER renamed to REACT_APP_HASH_ROUTER (#37)

--- src/components/EnvSpecificRouter.js
+++ src/components/EnvSpecificRouter.js
@@ -3,6 +3,6 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 // fix routing on github pages
 // for BrowserRouter server for all request should return index page
 export default function EnvSpecificRouter({ children }) {
-	const Router = process.env.HASH_ROUTER === 'true' ? HashRouter : BrowserRouter;
+	const Router = process.env.REACT_APP_HASH_ROUTER === 'true' ? HashRouter : BrowserRouter;
 	return <Router basename={process.env.PUBLIC_URL}>{children}</Router>;
 }
