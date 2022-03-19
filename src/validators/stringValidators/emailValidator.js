c28f6030349d1a67dfa39658da45342487df746b
Juliana Samoylenko <yuliana.samoilenko@flatstack.com>
2022-03-19 13:51:26 +0300

    change email regexp - allow numbers and characters

--- src/validators/stringValidators/emailValidator.js
+++ src/validators/stringValidators/emailValidator.js
@@ -1,4 +1,5 @@
-const emailRegExp = /^([a-zA-Z.]+)@([a-zA-Z]+)\.([a-zA-Z]+)$/;
+const emailRegExp =
+	/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export default function emailValidator(value) {
 	return value === '' || emailRegExp.test(value) ? '' : 'Wrong email format';
