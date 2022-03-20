c1890e53e4db5735587de00e9aaa23ebd8a85378
DmitriyFirsov <dmitriy.firsov@flatstack.com>
2022-03-20 14:08:23 +0500

    named qgl request

--- src/api/mutations/signIn.js
+++ src/api/mutations/signIn.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 const SIGN_IN_MUTATION = gql`
-	mutation ($login: String!, $password: String!) {
+	mutation signIn($login: String!, $password: String!) {
 		signin(input: { email: $login, password: $password }) {
 			accessToken
 			me {
--- src/api/mutations/signUp.js
+++ src/api/mutations/signUp.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 const SIGN_UP_MUTATION = gql`
-	mutation ($email: String!, $password: String!, $firstName: String, $lastName: String) {
+	mutation signUp($email: String!, $password: String!, $firstName: String, $lastName: String) {
 		signup(input: { email: $email, password: $password, firstName: $firstName, lastName: $lastName }) {
 			accessToken
 			refreshToken
